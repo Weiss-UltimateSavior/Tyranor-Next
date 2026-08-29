01a4b4b02f0d05ea03a98b5f19017a40f171d93b
Weiss-UltimateSavior <weiss450006@gmail.com>
2026-08-29 20:08:41 +0800

    build(app): 更新版本号到1.26
    仅修改了app模块的gradle配置文件，将应用版本名称从1.25调整为1.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,7 +111,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 2
-        versionName = "1.25"
+        versionName = "1.26"
         buildConfigField("String", "HIKARINAGI_CLIENT_ID", hikarinagiClientId.asBuildConfigString())
     }
 
